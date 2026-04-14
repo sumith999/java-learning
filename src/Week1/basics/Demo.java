2b459828e7d8ad0e07af12cc6b44baf01f96e5f7
sumith999 <sumithpoojary.ijs@gmail.com>
2026-04-14 12:21:43 +0530

    Initial commit

--- src/Week1/basics/Demo.java
+++ src/Week1/basics/Demo.java
@@ -1,12 +1,15 @@
 package Week1.basics;
 class Calculator{
-
+    public int add(int n1,int n2){
+        return n1 + n2;
     }
+}
 
 public class Demo {
     public static void main(String[] args){
         Calculator obj = new Calculator();
-
+        int result = obj.add(4,7);
+        System.out.println(result);
 
     }
 }

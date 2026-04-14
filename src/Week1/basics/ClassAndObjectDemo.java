1717fd596046c3bc842f6d1b8334e7dde6afe9b7
sumith999 <sumithpoojary.ijs@gmail.com>
2026-04-14 12:37:05 +0530

    Initial commit

--- src/Week1/basics/ClassAndObjectDemo.java
+++ src/Week1/basics/ClassAndObjectDemo.java
@@ -1,11 +1,12 @@
 package Week1.basics;
+
 class Calculator{
     public int add(int n1,int n2){
         return n1 + n2;
     }
 }
 
-public class Demo {
+public class ClassAndObjectDemo {
     public static void main(String[] args){
         Calculator obj = new Calculator();
         int result = obj.add(4,7);
